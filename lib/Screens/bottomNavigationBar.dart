3aeb53d3276eccf1a5b24730dc916f3746c989dd
Sneha Agrawal <snehaagrawal@gmail.com>
2021-09-07 09:19:24 +0530

    final commit

---
+++ lib/Screens/bottomNavigationBar.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+import 'package:homelabz/Screens/homeScreen.dart';
+import 'package:homelabz/components/colorValues.dart';
+
+class BottomNavigation extends StatefulWidget {
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return ConvexAppBar(
+          color: Color(ColorValues.THEME_COLOR),
+          backgroundColor: Color(ColorValues.THEME_COLOR),
+          items: [
+            TabItem(icon: Icons.home, title: "Home"),
+          ],
+      onTap: (int i){
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomeScreen()));
+      },
+    );
+
+  }
+}
